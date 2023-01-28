1ea4a74f4d5fc4120ae62b050a4877c5f165a0bc
pradthana7 <pradthana797@gmail.com>
2023-01-28 23:05:59 +0700

    scholarship - close #153

--- backend/controller/ScholarshipAp.go
+++ backend/controller/ScholarshipAp.go
@@ -1,10 +1,10 @@
 package controller
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 	"github.com/sut65/team06/entity"
+	"net/http"
+
 )
 
 func CreateScholarshipAp(c *gin.Context) {
@@ -46,13 +46,16 @@ func CreateScholarshipAp(c *gin.Context) {
 
 	// : สร้าง entity Scholarship applicant
 	Data_ScholarshipAp := entity.SCHOLARSHIPAP{
-		Student_Identity_Card: ScholarshipAp.Student_Identity_Card,
+		Identity_Card: ScholarshipAp.Identity_Card,
 		Reasons:               ScholarshipAp.Reasons,
 		GPAX:                  ScholarshipAp.GPAX,
 
-		Institute: Institute,
-		StudentID: ScholarshipAp.StudentID,
-		Student:   Student,
+		Scholarship:     Scholarship,
+		ScholarshipType: ScholarshipType,
+		Branch:          Branch,
+		Institute:       Institute,
+		StudentID:       ScholarshipAp.StudentID,
+		Student:         Student,
 	}
 
 	// : บันทึก
@@ -82,7 +85,7 @@ func ListScholarshipApTable(c *gin.Context) {
 // ดึงข้อมูล scholarship applicant by id
 func ListScholarshipApByID(c *gin.Context) {
 
-	var ScholarshipAp []entity.SCHOLARSHIPAP
+	var ScholarshipAp entity.SCHOLARSHIPAP
 	id := c.Param("id")
 	if err := entity.DB().Raw("SELECT * FROM scholarsh_ip_aps WHERE id = ?", id).Scan(&ScholarshipAp).Error; err != nil {
 
@@ -108,26 +111,108 @@ func DeleteScholarshipApByID(c *gin.Context) {
 
 // แก้ไขข้อมูล Scholarship Applicants
 func UpdateScholarshipAp(c *gin.Context) {
+	var Student entity.STUDENT
+	var Institute entity.INSTITUTE
+	var Branch entity.BRANCH
+	var Scholarship entity.SCHOLARSHIP
+	var ScholarshipType entity.SCHOLARSHIPTYPE
 	var ScholarshipAp entity.SCHOLARSHIPAP
-	var NewScholarshipAp entity.SCHOLARSHIPAP
+
 	if err := c.ShouldBindJSON(&ScholarshipAp); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		c.JSON(http.StatusBadRequest, gin.H{"ShouldBindJSON_ScholarshipAp_error": err.Error()})
 		return
 	}
 
-	NewScholarshipAp.Student_Identity_Card = ScholarshipAp.Student_Identity_Card
-	NewScholarshipAp.Reasons = ScholarshipAp.Reasons
-	NewScholarshipAp.GPAX = ScholarshipAp.GPAX
+	// : ค้นหา scholarship type ด้วย id
+	if tx := entity.DB().Where("id = ?", ScholarshipAp.ScholarshipTypeID).First(&ScholarshipType); tx.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "ScholarshipType not found"})
+		return
+	}
+	// : ค้นหา scholarship ด้วย id
+	if tx := entity.DB().Where("id = ?", ScholarshipAp.ScholarshipID).First(&Scholarship); tx.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Scholarship not found"})
+		return
+	}
 
-	NewScholarshipAp.Institute = ScholarshipAp.Institute
-	NewScholarshipAp.Branch = ScholarshipAp.Branch
-	NewScholarshipAp.Student = ScholarshipAp.Student
-	NewScholarshipAp.Scholarship = ScholarshipAp.Scholarship
-	NewScholarshipAp.ScholarshipType = ScholarshipAp.ScholarshipType
+	// : ค้นหา institute ด้วย id
+	if tx := entity.DB().Where("id = ?", ScholarshipAp.InstituteID).First(&Institute); tx.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Institute not found"})
+		return
+	}
 
-	if err := entity.DB().Where("id = ?", ScholarshipAp.ID).Updates(&NewScholarshipAp).Error; err != nil {
+	// : ค้นหา branch ด้วย id
+	if tx := entity.DB().Where("id = ?", ScholarshipAp.BranchID).First(&Branch); tx.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Branch not found"})
+		return
+	}
+
+	update := entity.SCHOLARSHIPAP{
+		Identity_Card: ScholarshipAp.Identity_Card,
+		Reasons:               ScholarshipAp.Reasons,
+		GPAX:                  ScholarshipAp.GPAX,
+
+		Scholarship:     Scholarship,
+		ScholarshipType: ScholarshipType,
+		Branch:          Branch,
+		Institute:       Institute,
+		StudentID:       ScholarshipAp.StudentID,
+		Student:         Student,
+	}
+
+	if err := entity.DB().Where("id = ?", ScholarshipAp.ID).Updates(&update).Error; err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	c.JSON(http.StatusOK, gin.H{"data": NewScholarshipAp})
+	c.JSON(http.StatusOK, gin.H{"data": ScholarshipAp})
+}
+
+// ดึงข้อมูล scholarship by id
+func ListScholarshipByID(c *gin.Context) {
+	var Scholarship entity.SCHOLARSHIP
+	id := c.Param("id")
+
+	if tx := entity.DB().Where("id = ?", id).First(&Scholarship); tx.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Scholarship not found"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": Scholarship})
+}
+
+// ดึงข้อมูล scholarship type by id
+func ListScholarshipTypeByID(c *gin.Context) {
+
+	var ScholarshipType entity.SCHOLARSHIPTYPE
+	id := c.Param("id")
+	if tx := entity.DB().Where("id = ?", id).First(&ScholarshipType); tx.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "ScholarshipType not found"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": ScholarshipType})
+}
+
+func GetApplicantByID(c *gin.Context) {
+
+	var Applicants entity.SCHOLARSHIPAP
+	id := c.Param("id")
+	if tx := entity.DB().Preload("ScholarshipType").Preload("Scholarship").Preload("Branch").Preload("Institute").Preload("Student").Where("id = ?", id).First(&Applicants); tx.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Applicants not found"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": Applicants})
+}
+
+// get student
+func GetStudentByID(c *gin.Context) {
+
+	var Student entity.STUDENT
+	id := c.Param("id")
+	if tx := entity.DB().Preload("Student").Where("id = ?", id).First(&Student); tx.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Student not found"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": Student})
 }
