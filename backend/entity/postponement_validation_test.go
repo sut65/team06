cebbc7398265953eb6902210e554023cb43932fd
bxngearnx <b6321437@g.sut.ac.th>
2023-02-08 13:17:44 +0700

    add unit test postponement - close #168

---
+++ backend/entity/postponement_validation_test.go
@@ -0,0 +1,83 @@
+package entity
+
+import (
+	"testing"
+	"time"
+
+	"github.com/asaskevich/govalidator"
+	. "github.com/onsi/gomega"
+)
+
+func Test_Validate_Postponement_Student_Number(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	postponement := POSTPONEMENT{
+		Postponement_Student_Number: "F63000",
+		Postponement_Student_Name:   "ศุภกานต์ แสงจันทร์",
+		Postponement_AcademicYear:   "2565",
+		Postponement_Gpax:           "4.00",
+		Postponement_Credit:         "22",
+		Postponement_Date:           time.Now(),
+		Postponement_Reasons:        "เนื่องจากทางบ้านมีปัญหาเรื่องการเงิน",
+	}
+	// ตรวจสอบด้วย govalidator
+	ok, err := govalidator.ValidateStruct(postponement)
+
+	// ok ต้องไม่เป็นค่า true แปลว่าต้องจับ error ได้
+	g.Expect(ok).NotTo(BeTrue())
+
+	// err ต้องไม่เป็นค่า nil แปลว่าต้องจับ error ได้
+	g.Expect(err).NotTo(BeNil())
+
+	// err.Error ต้องมี error message แสดงออกมา
+	g.Expect(err.Error()).To(Equal("Postponement_Student_Number: F63000 does not validate as matches(^[BMD]\\d{7}$)"))
+}
+func Test_Validate_Postponement_Student_Name(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	postponement := POSTPONEMENT{
+		Postponement_Student_Number: "B6300000",
+		Postponement_Student_Name:   "",
+		Postponement_AcademicYear:   "2565",
+		Postponement_Gpax:           "4.00",
+		Postponement_Credit:         "22",
+		Postponement_Date:           time.Now(),
+		Postponement_Reasons:        "เนื่องจากทางบ้านมีปัญหาเรื่องการเงิน",
+	}
+	// ตรวจสอบด้วย govalidator
+	ok, err := govalidator.ValidateStruct(postponement)
+
+	// ok ต้องไม่เป็นค่า true แปลว่าต้องจับ error ได้
+	g.Expect(ok).NotTo(BeTrue())
+
+	// err ต้องไม่เป็นค่า nil แปลว่าต้องจับ error ได้
+	g.Expect(err).NotTo(BeNil())
+
+	// err.Error ต้องมี error message แสดงออกมา
+	g.Expect(err.Error()).To(Equal("name cannot be blank"))
+}
+
+func Test_Validate_Postponement_Reasons(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	postponement := POSTPONEMENT{
+		Postponement_Student_Number: "B6300000",
+		Postponement_Student_Name:   "ศุภกานต์ แสงจันทร์",
+		Postponement_AcademicYear:   "2565",
+		Postponement_Gpax:           "4.00",
+		Postponement_Credit:         "22",
+		Postponement_Date:           time.Now(),
+		Postponement_Reasons:        "เน",
+	}
+	// ตรวจสอบด้วย govalidator
+	ok, err := govalidator.ValidateStruct(postponement)
+
+	// ok ต้องไม่เป็นค่า true แปลว่าต้องจับ error ได้
+	g.Expect(ok).NotTo(BeTrue())
+
+	// err ต้องไม่เป็นค่า nil แปลว่าต้องจับ error ได้
+	g.Expect(err).NotTo(BeNil())
+
+	// err.Error ต้องมี error message แสดงออกมา
+	g.Expect(err.Error()).To(Equal("กรุณากรอกเหตุผลอย่างน้อย10ตัวอักษร"))
+}
