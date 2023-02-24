e454b0259cdc9653cde03d8bc7f23c8e3bb2d0c7
bxngearnx <b6321437@g.sut.ac.th>
2023-02-24 14:26:54 +0700

    commit add positive postponement  test - close #233

--- backend/entity/postponement_validation_test.go
+++ backend/entity/postponement_validation_test.go
@@ -81,3 +81,25 @@ func Test_Validate_Postponement_Reasons(t *testing.T) {
 	// err.Error ต้องมี error message แสดงออกมา
 	g.Expect(err.Error()).To(Equal("Postponement_Reasons: เน does not validate as minstringlength(10)"))
 }
+func Test_Postponementr(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	postponement := POSTPONEMENT{
+		Postponement_Student_Number: "B6300000",
+		Postponement_Student_Name:   "ศุภกานต์ แสงจันทร์",
+		Postponement_AcademicYear:   "2565",
+		Postponement_Gpax:           "4.00",
+		Postponement_Credit:         "22",
+		Postponement_Date:           time.Now(),
+		Postponement_Reasons:        "เนื่องจากทางบ้านมีปัญหาเรื่องการเงิน",
+	}
+	// ตรวจสอบด้วย govalidator
+	ok, err := govalidator.ValidateStruct(postponement)
+
+	// ok ต้องไม่เป็นค่า true แปลว่าต้องจับ error ได้
+	g.Expect(ok).To(BeTrue())
+
+	// err ต้องไม่เป็นค่า nil แปลว่าต้องจับ error ได้
+	g.Expect(err).To(BeNil())
+
+}
